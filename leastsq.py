2fa70d827df78e93b111bc856de90573bdf94ba9
scaactk <tjut_ljx@yeah.net>
2018-12-27 14:31:49 +0800

    leastsq

--- leastsq.py
+++ leastsq.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import leastsq
 
 
 # target func
@@ -32,3 +34,26 @@ x_points = np.linspace(0, 1, 1000)
 y_ = real_func(x)
 y = [np.random.normal(0, 0.1) + y1 for y1 in y_]
 
+
+def fitting(M=0):
+    """
+    :param M: 多项式次数
+    :return:
+    """
+
+    # 随机初始化 polynomial 参数
+    p_init = np.random.rand(M + 1)
+
+    # 最小二乘法
+    p_lsq = leastsq(residuals_func, p_init, args=(x, y))
+    print("Fitting Parameters:", p_lsq[0])
+
+    # 可视化
+    plt.plot(x_points, real_func(x_points), label='real')
+    plt.plot(x_points, fit_func(p_lsq[0], x_points), label='fitted curve')
+    plt.plot(x, y, 'bo', label='noise')
+    plt.legend()
+    return p_lsq
+
+
+p_lsq_0 = fitting(M=1)
